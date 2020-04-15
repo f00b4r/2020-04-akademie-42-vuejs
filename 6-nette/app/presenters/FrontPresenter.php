d0159bf06f4d513aacb50499337d8ae397ef9162
Milan Felix Šulc <rkfelix@gmail.com>
2020-04-15 15:42:52 +0200

    Progress: nette

--- 6-nette/app/presenters/FrontPresenter.php
+++ 6-nette/app/presenters/FrontPresenter.php
@@ -4,4 +4,11 @@ namespace App\Presenters;
 
 final class FrontPresenter extends BasePresenter
 {
+
+    public function actionDefault() {
+        $this->template->SSR = [
+            ['id' => 1, 'name' => 'Felix']
+        ];
+    }
+
 }
